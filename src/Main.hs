b838a9bb2e50979aee3a18eef8252e098bede5a4
deweyvm <deweyvm@gmail.com>
2013-11-02 13:47:22 -0400

    playing with toy language

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,7 @@ import Pone.Option
 import System.IO
 import Data.Monoid
 import Pone.RandomStuff
-    
+import qualified Data.Map as Map
 
 import Control.Applicative((<*))
 import Text.Parsec
@@ -21,17 +21,23 @@ import Text.Parsec.Language
 {-
 Grammar:
 
-<expr>    ::= <letexpr>
+
+<expr>    ::= <definition-expr>
+            | <procedure-expr>
             | "(" <expr> ")"
             | <expr> <binop> <expr>
+            | <procedure>
             | <ident>
             | <integer>
-           
-<letexpr> ::= "let" <ident> "be" <expr> "in" <expr>
             
-<binop>   ::= "+" | "*"
+           
+<definition-expr> ::= "define" <ident> "as" <expr> "in" <expr>
 
-<ident>   ::= [a-zA-Z][\w]*
+<procedure> ::= <ident> <arg> (<arg> *) "is" <expr>
+       
+<binop> ::= "+" | "*"
+
+<ident> ::= [a-zA-Z][\w]*
 
 <integer> ::= [0-9]+
 
@@ -39,7 +45,13 @@ Grammar:
 
 
 data Op = Plus | Times deriving Show
-data Expr = Assign String Expr Expr | Value Integer | Binop Op Expr Expr | Paren Expr | Identifer String deriving Show
+data Expr = Assign String Expr Expr 
+          | Value Integer 
+          | Binop Op Expr Expr 
+          | Paren Expr 
+          | Identifier String
+          | Procedure String [String] Expr Expr
+    deriving Show
 
 languageDef = emptyDef{ commentStart = "<"
                       , commentEnd = ">"
@@ -48,7 +60,7 @@ languageDef = emptyDef{ commentStart = "<"
                       , opStart = oneOf "+*"
                       , opLetter = oneOf "+*"
                       , reservedOpNames = ["+", "*"]
-                      , reservedNames = ["let", "be", "in"]
+                      , reservedNames = ["define", "procedure", "as", "in", "is"]
                       }
     
 TokenParser{ parens = m_parens
@@ -67,22 +79,64 @@ exprParser = buildExpressionParser opTable term <?> "expression"
     
 term = m_parens exprParser
        <|> fmap Value m_number
-       <|> fmap Identifer m_identifier
-       <|> do { m_reserved "let"
+       <|> fmap Identifier m_identifier
+       <|> do { m_reserved "define"
               ; name <- m_identifier
-              ; m_reserved "be"
+              ; m_reserved "as"
               ; value <- exprParser
               ; m_reserved "in"
               ; expr <- exprParser
               ; return $ Assign name value expr
               }
+       <|> do { m_reserved "procedure"
+              ; name <- m_identifier
+              ; args <- argParser
+              ; m_reserved "is"
+              ; value <- exprParser
+              ; m_reserved "in"
+              ; expr <- exprParser
+              ; return $ Procedure name args value expr
+              }
+       
+spaceSep1 p = sepBy1 p m_whiteSpace
+       
+argParser :: Parser [String]
+argParser = fmap (id) (spaceSep1 m_identifier)
+       
        
 mainParser :: Parser Expr
 mainParser = m_whiteSpace >> exprParser <* eof
-    
-input = "let a be (1 + (2 * 4)) in a" 
+          
+type Environment = Map.Map String Integer
 
-main = case parse mainParser "" input of 
+pushName :: Environment -> Integer -> String -> Environment
+pushName env value name = Map.insert name value env
+
+lookupName :: Environment -> String -> Integer
+lookupName env name = env Map.! name --fixme, handle unbound names
+
+eval :: Environment -> Expr -> Integer
+eval env (Assign name v e) = let value = eval env v
+                                 newState = pushName env value name 
+                              in eval newState e
+eval env (Value i) = i
+eval env (Binop op e1 e2) = let v1 = eval env e1
+                                v2 = eval env e2
+                             in case op of 
+                                 Plus -> v1 + v2
+                                 Times -> v1 * v2
+eval env (Paren e) = eval env e
+eval env (Identifier s) = lookupName env s
+eval env (Procedure name args value expr) = eval env expr
+   
+-- change map to (implicit) stack of maps
+main = do
+    source <- readFile "C:/Users/M/Desktop/pone/pone_src/test.pone"
+    case parse mainParser "" source of 
        Left err -> print err
-       Right ans -> print ans
+       Right ans -> print $ eval (Map.empty) ans
     
+
+
+
+       
