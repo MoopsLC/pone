40b05ea6293dcdf5233a2a02fa31559d64a4f46d
deweyvm <deweyvm@gmail.com>
2013-11-04 18:46:46 -0500

    implemented global procedures and constants

--- src/Pone/Interpreter.hs
+++ src/Pone/Interpreter.hs
@@ -8,7 +8,7 @@ import Pone.Parser
           
 data ProcedureDef = ProcedureDef String [String] Expr
 type ProcedureMap = Map.Map String ProcedureDef
-type Environment = Map.Map String ProcedureDef
+type Environment = Map.Map String Integer
 
 pushDef :: ProcedureMap -> String -> ProcedureDef -> ProcedureMap
 pushDef map name def = Map.insert name def map
@@ -23,20 +23,29 @@ lookupName :: Environment -> String -> Integer
 lookupName env name = env Map.! name --fixme, handle unbound names
 
 poneEval :: PoneProgram -> Integer
-poneEval (Program globals expr) = let (env, procs) = loadGlobalProcs globals
-                                  in eval Map.empty procs expr
+poneEval (Program globals expr) = 
+    let (env, procs) = foldl (\acc def -> bind acc def) (Map.empty, Map.empty) globals
+    in eval env procs expr
+    
 
-def2StringProc :: GlobalDef -> Maybe (String, ProcedureDef) --DONT use maybe here, pattern match one level higher than this
-def2StringProc (GlobalProcedureBind (ProcedureBind name params value)) = 
-    Just (name, ProcedureDef name params value)
-def2StringProc (GlobalIdentifierBind _ ) = Nothing
---def2StringProc _ = undefined
 
-[T] -> 
 
-loadGlobalProcs :: [GlobalDef] -> (Environment, ProcedureMap)
-loadGlobalProcs defs = mapFold Map.empty (map def2StringProc) defs
+bind :: (Environment,ProcedureMap) -> GlobalDef -> (Environment, ProcedureMap)
+bind (env, procs) def = case def of
+    GlobalProcedureBind (ProcedureBind name parameters body) ->
+        (env, pushDef procs name (ProcedureDef name parameters body))
+    GlobalIdentifierBind (IdentifierBind name value) -> 
+        let evaluated = eval env procs value in
+        ((pushName env name evaluated), procs)
+       
 
+
+mapFold :: Ord k => Map.Map k v -> [(k, v)] -> Map.Map k v
+mapFold map [] = map
+mapFold map ((param, value):xs) = mapFold (Map.insert param value map) xs
+
+
+    
 eval :: Environment -> ProcedureMap -> Expr -> Integer
 eval env procs expr = case expr of
     Value (PoneInteger i) -> i 
@@ -47,7 +56,6 @@ eval env procs expr = case expr of
             Plus -> v1 + v2
             Times -> v1 * v2
                                  
-    IdentifierEval s -> lookupName env s
     LocalIdentifierBind (IdentifierBind name v) e -> 
         let value = eval env procs v
             newState = pushName env name value
@@ -55,6 +63,7 @@ eval env procs expr = case expr of
     LocalProcedureBind (ProcedureBind name args value) expr ->  --make sure no repeat args
         let newProcs = pushDef procs name (ProcedureDef name args value) in 
         eval env newProcs expr
+    IdentifierEval s -> lookupName env s
     ProcedureEval name args -> 
         let evaluated :: [Integer] = map (eval env procs) args
             procc = lookupDef procs name
@@ -64,8 +73,3 @@ eval env procs expr = case expr of
                     newEnv :: Environment = mapFold env zipped 
                 in eval newEnv procs expr
                 
-
-
-mapFold :: Ord k => Map.Map k v -> [(k, v)] -> Map.Map k v
-mapFold map [] = map
-mapFold map ((param, value):xs) = mapFold (Map.insert param value map) xs
