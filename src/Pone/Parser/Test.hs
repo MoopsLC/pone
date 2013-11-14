795307ca8ae15c47b2b94a3e7bd26393a4368855
deweyvm <deweyvm@gmail.com>
2013-11-14 17:32:17 -0500

    moved file

---
+++ src/Pone/Parser/Test.hs
@@ -0,0 +1,52 @@
+module Pone.Parser.Test where
+
+import Pone.Parser.Type
+import Test.QuickCheck
+
+instance Arbitrary TypeRep where
+    arbitrary = sized sizedType
+
+
+sizedType 0 = genName
+sizedType n | n > 0 =
+    frequency [ (1, genArrow)
+              , (1, genName)
+              , (1, genApply)
+              ]
+  where sub = sizedType (n `div` 4)
+        genArrow = ArrowTR <$> ((<::>) <$> sub <*> sub <*> resize 4 (listOf1 (sub :: Gen TypeRep)))
+        genApply = ApplyTR <$> ((<::>) <$> sub <*> sub <*> resize 4 (listOf1 (sub :: Gen TypeRep)))
+sizedType _ = undefined
+
+arbitraryName :: Gen String
+arbitraryName = vectorOf 1 (elements "abcdefghijklmnopqrstuvwxyzABCDEFG")
+
+genName :: Gen TypeRep
+genName = NameTR <$> arbitraryName
+
+stripParens :: String -> String
+stripParens = stripOpen . stripClose
+
+stripOpen :: String -> String
+stripOpen ('(':xs) = stripOpen xs
+stripOpen x = x
+
+stripClose :: String -> String
+stripClose x = case reverse x of
+    (')':xs) -> stripClose (reverse xs)
+    x -> reverse x
+
+
+myTest :: TypeRep -> Bool
+myTest t =
+    let lhs :: String = stripParens (parsePoneType $ show t)
+        rhs :: String = stripParens (show t)
+    in
+        {-printInlineStr ("LHS: " ++ lhs ++ " \n" ++ "RHS: " ++ rhs)-} (lhs == rhs)
+
+propTrue :: Gen TypeRep -> Property
+propTrue prog = forAll prog myTest
+
+main :: IO ()
+main = do
+    quickCheck $ propTrue (arbitrary :: Gen TypeRep)
